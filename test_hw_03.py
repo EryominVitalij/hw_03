6f049fe169a0d0ba378eaee3cb9e58fbcf277015
Vitalij Eryomin <Eryomin@bk.ru>
2025-05-23 15:11:24 +0300

    search positive and negative

--- test_hw_03.py
+++ test_hw_03.py
@@ -1,10 +1,14 @@
-from selene import browser, be, have
+from selene import browser, be, have, by
 
-def test_01(open_browser):
+def test_01():
+    browser.open('https://google.com')
     browser.element('[name = "q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('html').should(have.text('Об этой странице'))
 
-def test_02(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('q12w3e4r5t555').press_enter()
-    browser.element('[class="card-section"]').should(have.text('Поиск по запросу q12w3e4r5t555 не принёс результатов.'))
+# по google не получилось выполнить поиск абракадабры, капча мешается, как обойти пока не понимаю
+
+def test_02():
+    browser.open('https://www.bing.com/')
+    browser.element('[name="q"]').should(be.blank).type('pokjQW87888').press_enter()
+    browser.element('[id="b_results"]').should(have.text('Не удалось найти ни одного результата для pokjQW87888'))
 
