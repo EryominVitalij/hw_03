97bedf65354f5d492afab2bbc63e2f98b8ca86ac
Vitalij Eryomin <Eryomin@bk.ru>
2025-05-23 17:41:43 +0300

    переименовал названия тестов на более понятные

--- test_hw_03.py
+++ test_hw_03.py
@@ -1,13 +1,13 @@
 from selene import browser, be, have
 
-def test_01():
+def test_search_positive():
     browser.open('https://google.com')
     browser.element('[name = "q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('html').should(have.text('Об этой странице'))
 
 # по google не получилось выполнить поиск абракадабры, капча мешается, как обойти пока не понимаю
 
-def test_02():
+def test_search_nothing_found():
     browser.open('https://www.bing.com/')
     browser.element('[name="q"]').should(be.blank).type('pokjQW87888').press_enter()
     browser.element('[id="b_results"]').should(have.text('Не удалось найти ни одного результата для pokjQW87888'))
